dec30a5dc7218ca462039e529314145ee20d5a65
DanHausen <danihausen@live.com>
2021-06-25 23:26:02 -0300

    main

---
+++ Python/Answers/Calculating with Functions.py
@@ -0,0 +1,16 @@
+def zero(): 
+def one(): 
+def two(): 
+def three(): 
+def four(): 
+def five(): 
+def six(): 
+def seven(): 
+def eight(): 
+def nine(): 
+    return 9
+
+def plus(): 
+def minus(): 
+def times(): 
+def divided_by(): 
