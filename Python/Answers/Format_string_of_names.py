42c0f08fd3c4645aa3889ce88f55b7cfb675c281
DanHausen <danihausen@live.com>
2021-07-08 23:03:36 -0300

    main

---
+++ Python/Answers/Format_string_of_names.py
@@ -0,0 +1,16 @@
+#https://www.codewars.com/kata/53368a47e38700bd8300030d/train/python
+# Given: an array containing hashes of names
+
+# Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+# Example:
+# namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])
+# # returns 'Bart, Lisa & Maggie'
+# namelist([ {'name': 'Bart'}, {'name': 'Lisa'} ])
+# # returns 'Bart & Lisa'
+# namelist([ {'name': 'Bart'} ])
+# # returns 'Bart'
+# namelist([])
+# # returns ''
+# Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
