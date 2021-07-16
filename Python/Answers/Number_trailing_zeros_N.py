aaab0fac8bc97d1af943d2ff30ae987db3409cf2
DanHausen <danihausen@live.com>
2021-07-15 22:37:25 -0300

    main

--- Python/Answers/Number_trailing_zeros_N.py
+++ Python/Answers/Number_trailing_zeros_N.py
@@ -16,3 +16,26 @@
 # # 12! = 479001600 --> 2 trailing zeros
 # Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
 
+def zeros(n):
+    count = 0
+    if n<0:
+        return -1
+    while n >= 5:
+        n //= 5
+        count += n
+    return count
+
+#BEST PRACTICES:
+# def zeros(n):
+#   x = n/5
+#   return x+zeros(x) if x else 0
+
+# def zeros(n):
+#     return n/5 + zeros(n/5) if n >= 5 else 0
+
+# def zeros(n):
+#     return 0 if int(n/5) < 1 else int(n/5) + int(zeros(n/5))
+
+# import math
+# def zeros(n):
+#   return reduce(lambda p,c: p + n // 5**c, range(1,13), 0)
