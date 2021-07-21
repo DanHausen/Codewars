88bdaf082d58d2729a2cea2229e9788247811efc
DanHausen <danihausen@live.com>
2021-07-20 23:09:49 -0300

    main

--- Python/Answers/Convert_string_to_camelCase.py
+++ Python/Answers/Convert_string_to_camelCase.py
@@ -5,3 +5,31 @@
 # "the-stealth-warrior" gets converted to "theStealthWarrior"
 # "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 
+
+def to_camel_case(text):
+    if text == '':
+        return ''
+    else:
+        l = list(text)
+        f = ''
+        for i in range(len(l)-1):
+               if l[i] == '-' or l[i] == '_':
+                    l[i+1] = l[i+1].upper()
+        for j in l:
+            if j != '_' and j != '-':
+                f += j
+        return f
+    
+#BEST PRACTICES:
+# def to_camel_case(s):
+#     return s[0] + s.title().translate(None, "-_")[1:] if s else s
+
+
+# from re import compile as reCompile
+# PATTERN = reCompile(r'(?i)[-_]([a-z])')
+# def to_camel_case(text):
+#     return PATTERN.sub(lambda m: m.group(1).upper(), text)
+
+
+# def to_camel_case(text):
+#     return "".join([i if n==0 else i.capitalize() for n,i in enumerate(text.replace("-","_").split("_"))])
